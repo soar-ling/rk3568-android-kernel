c9a88b9161ce8b86311184214511f75354587c3d
zsl <zsl@skysi.com>
2023-07-04 18:41:01 +0800

    rk3568 S1 multi dtb & multi logo for one firemware multi board
    Change-Id: I6ef9f3567ab5c413f6c707e6b2da8359b3f29bec

--- scripts/mkmultidtb.py
+++ scripts/mkmultidtb.py
@@ -26,6 +26,11 @@ DTBS['RK3308-EVB'] = OrderedDict([('rk3308-evb-dmic-i2s-v10', '#_saradc_ch3=288'
 				  ('rk3308-evb-dmic-pdm-v10', '#_saradc_ch3=1024'),
 				  ('rk3308-evb-amic-v10', '#_saradc_ch3=407')])
 
+DTBS['RK3568-EVB'] = OrderedDict([('rk3568-iot-lp4x-s1-v10-rk628-gvi', '#_saradc_ch3=1023'),
+				  ('rk3568-iot-lp4x-s1-v10-rk628-gvi-down', '#_saradc_ch3=945'),
+				  ('rk3568-iot-lp4x-s1-v11-lvds', '#_saradc_ch3=860'),
+				  ('rk3568-iot-lp4x-s1-v11-lvds-right', '#_saradc_ch3=781')])
+
 def main():
     if (len(sys.argv) < 2) or (sys.argv[1] == '-h'):
         print __doc__
@@ -48,7 +53,7 @@ def main():
         target_dtb_list += ' ' + new_file
 
     print target_dtb_list
-    os.system('scripts/resource_tool logo.bmp logo_kernel.bmp ' + target_dtb_list)
+    os.system('scripts/resource_tool logo_270.bmp logo.bmp logo_90.bmp logo_180.bmp ' + target_dtb_list)
     os.system('rm ' + target_dtb_list)
 
 if __name__ == '__main__':
