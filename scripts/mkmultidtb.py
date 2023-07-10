3966140bafa9ca1799df51e07848084a36ca23b8
zsl <zsl@skysi.com>
2023-07-10 17:38:00 +0800

    rk3568 S1 auto match board dts list update
    Change-Id: Ic8c7990f9dd9bd298b0917a224050b07c998589c

--- scripts/mkmultidtb.py
+++ scripts/mkmultidtb.py
@@ -26,8 +26,9 @@ DTBS['RK3308-EVB'] = OrderedDict([('rk3308-evb-dmic-i2s-v10', '#_saradc_ch3=288'
 				  ('rk3308-evb-dmic-pdm-v10', '#_saradc_ch3=1024'),
 				  ('rk3308-evb-amic-v10', '#_saradc_ch3=407')])
 
-DTBS['RK3568-EVB'] = OrderedDict([('rk3568-iot-lp4x-s1-v10-rk628-gvi', '#_saradc_ch3=1023'),
-				  ('rk3568-iot-lp4x-s1-v10-rk628-gvi-down', '#_saradc_ch3=945'),
+DTBS['RK3568-EVB'] = OrderedDict([('rk3568-iot-lp4x-s1-v10-rk628-gvi', '#_saradc_ch1=1023#_saradc_ch3=1023'),
+				  ('rk3568-iot-lp4x-s1-v11-rk628-gvi', '#_saradc_ch3=1023'),
+				  ('rk3568-iot-lp4x-s1-v11-rk628-gvi-down', '#_saradc_ch3=945'),
 				  ('rk3568-iot-lp4x-s1-v11-lvds', '#_saradc_ch3=860'),
 				  ('rk3568-iot-lp4x-s1-v11-lvds-right', '#_saradc_ch3=781')])
 
