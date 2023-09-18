96a108e5e699612f36027f6d8289e6ea0bd0cd1c
Liang Chen <cl@rock-chips.com>
2023-09-18 10:34:50 +0800

    soc: rockchip: cpuinfo: Add support for rk3567
    Change-Id: I8db6a404e0c784cb797c4e5a7849ae73089726af
    Signed-off-by: Liang Chen <cl@rock-chips.com>

--- include/linux/rockchip/cpu.h
+++ include/linux/rockchip/cpu.h
@@ -25,6 +25,7 @@
 #define ROCKCHIP_CPU_RK3288		0x32880000
 #define ROCKCHIP_CPU_RK3308		0x33080000
 #define ROCKCHIP_CPU_RK3566		0x35660000
+#define ROCKCHIP_CPU_RK3567		0x35670000
 #define ROCKCHIP_CPU_RK3568		0x35680000
 
 #if IS_ENABLED(CONFIG_ROCKCHIP_CPUINFO)
@@ -161,6 +162,13 @@ static inline bool cpu_is_rk3566(void)
 	return of_machine_is_compatible("rockchip,rk3566");
 }
 
+static inline bool cpu_is_rk3567(void)
+{
+	if (rockchip_soc_id)
+		return (rockchip_soc_id & ROCKCHIP_CPU_MASK) == ROCKCHIP_CPU_RK3567;
+	return of_machine_is_compatible("rockchip,rk3567");
+}
+
 static inline bool cpu_is_rk3568(void)
 {
 	if (rockchip_soc_id)
@@ -169,6 +177,7 @@ static inline bool cpu_is_rk3568(void)
 }
 #else
 static inline bool cpu_is_rk3566(void) { return false; }
+static inline bool cpu_is_rk3567(void) { return false; }
 static inline bool cpu_is_rk3568(void) { return false; }
 #endif
 
@@ -186,6 +195,7 @@ static inline bool cpu_is_rk3568(void) { return false; }
 #define ROCKCHIP_SOC_RK3308	(ROCKCHIP_CPU_RK3308 | 0x00)
 #define ROCKCHIP_SOC_RK3308B	(ROCKCHIP_CPU_RK3308 | 0x01)
 #define ROCKCHIP_SOC_RK3566	(ROCKCHIP_CPU_RK3566 | 0x00)
+#define ROCKCHIP_SOC_RK3567	(ROCKCHIP_CPU_RK3567 | 0x00)
 #define ROCKCHIP_SOC_RK3568	(ROCKCHIP_CPU_RK3568 | 0x00)
 
 #define ROCKCHIP_SOC(id, ID) \
@@ -209,6 +219,7 @@ ROCKCHIP_SOC(rk3288w, RK3288W)
 ROCKCHIP_SOC(rk3308, RK3308)
 ROCKCHIP_SOC(rk3308b, RK3308B)
 ROCKCHIP_SOC(rk3566, RK3566)
+ROCKCHIP_SOC(rk3567, RK3567)
 ROCKCHIP_SOC(rk3568, RK3568)
 
 #endif
