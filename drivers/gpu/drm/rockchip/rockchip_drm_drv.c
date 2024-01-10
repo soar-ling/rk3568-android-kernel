c3a7501fbb01078e2b2ca68e122ee6e8f1efd2d4
zsl <zhengsanle@skysi.con.cn>
2024-01-10 10:36:44 +0800

    Revert "fix GVI fd import null error"
    This reverts commit 47fcd3dd658d49a4680989d606a173ea627c28e1.

--- drivers/gpu/drm/rockchip/rockchip_drm_drv.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_drv.c
@@ -1721,7 +1721,7 @@ static int rockchip_drm_bind(struct device *dev)
 			break;
 
 		s = to_rockchip_crtc_state(crtc->state);
-		if (is_support_hotplug(s->output_type) && helper->fb)
+		if (is_support_hotplug(s->output_type))
 			drm_framebuffer_get(helper->fb);
 	}
 
